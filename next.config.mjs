420bdaff2627d41866390c5f214ccc44da26488b
md-ashikur <ashik76690@gmail.com>
2025-04-01 23:47:12 +0600

    deploy update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    reactStrictMode: true,
-    images: {
-        unoptimized: true, // Ensures static exports work properly
-      },
-    trailingSlash: true, // Helps with static hosting issues
+    // output: "export",
+    // reactStrictMode: true,
+    // images: {
+    //     unoptimized: true, // Ensures static exports work properly
+    //   },
+    // trailingSlash: true, // Helps with static hosting issues
 };
 
 export default nextConfig;
